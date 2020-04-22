cb353c3897810d9488efc2ea2d8269b380702865
Dragutin Todorovic <duda.todorovic.97@gmail.com>
2020-04-22 12:30:11 +0200

    Changed route for logger

--- backend/connection.php
+++ backend/connection.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL | E_STRICT);
 ini_set("display_errors", true);
 ini_set("log_errors", true);
-ini_set("error_log", "logger.log");
+ini_set("error_log", "backend/logger.log");
 
 $servername = "localhost";
 $username = "root";
